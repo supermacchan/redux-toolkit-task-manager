afc26900dc76411bb0f80638480cbc746407bddb
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-11 19:50:49 +0200

    проектирование в комменты

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+// const appState = {
+//     tasks: [
+//       { id: 0, text: "Learn HTML and CSS", completed: true },
+//       { id: 1, text: "Get good at JavaScript", completed: true },
+//       { id: 2, text: "Master React", completed: false },
+//       { id: 3, text: "Discover Redux", completed: false },
+//       { id: 4, text: "Build amazing apps", completed: false },
+//     ],
+//     filters: {
+//       status: "all",
+//     },
+//   };
