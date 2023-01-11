deab356248fc4cff8c5d86622e61ab0540f0d1fa
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-11 20:02:26 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,23 @@
-// const appState = {
-//     tasks: [
-//       { id: 0, text: "Learn HTML and CSS", completed: true },
-//       { id: 1, text: "Get good at JavaScript", completed: true },
-//       { id: 2, text: "Master React", completed: false },
-//       { id: 3, text: "Discover Redux", completed: false },
-//       { id: 4, text: "Build amazing apps", completed: false },
-//     ],
-//     filters: {
-//       status: "all",
-//     },
-//   };
+import { createStore } from "redux";
+
+// начальное значение state приложения
+const initialState = {
+    tasks: [
+        { id: 0, text: "Learn HTML and CSS", completed: true },
+        { id: 1, text: "Get good at JavaScript", completed: true },
+        { id: 2, text: "Master React", completed: false },
+        { id: 3, text: "Discover Redux", completed: false },
+        { id: 4, text: "Build amazing apps", completed: false },
+      ],
+      filters: {
+        status: "all",
+      },
+};
+
+// корневой редюсер - функция, принимает state, action
+const rootReducer = (state = initialState, action) => {
+    return state;
+};
+
+// создаем и экспортируем стор, передаем ему основной, корневой редюсер
+export const store = createStore(rootReducer);
