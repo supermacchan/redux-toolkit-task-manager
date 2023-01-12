3a5438d6886b3bd3613b10fc28209107cb83727d
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-12 21:23:04 +0200

    Update Task.jsx

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux";
-import { deleteTask, toggleCompleted } from "redux/actions";
+import { deleteTask, toggleCompleted } from "redux/tasksSlice";
 import { MdClose } from "react-icons/md";
 import css from "./Task.module.css";
 
