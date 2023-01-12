13fcaf511e9bfb82db9714c4e9f991625774c47d
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-12 21:56:15 +0200

    redux persist :)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from "./redux/store";
 import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store} >
+      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
         <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,48 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { tasksReducer } from "./tasksSlice";
 import { filtersReducer } from "./filtersSlice";
+import localStorage from 'redux-persist/lib/storage';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+
+const persistConfig = {
+    key: 'root/task-manager',
+    storage: localStorage,
+    blacklist: ['filter'],
+    stateReconciler: autoMergeLevel2,
+};
+
+const rootReducer = combineReducers({
+  tasks: tasksReducer,
+  filters: filtersReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    tasks: tasksReducer,
-    filters: filtersReducer,
-  }
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware => 
+      getDefaultMiddleware({
+          serializableCheck: {
+              ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+      })
 });
+
+export const persistor = persistStore(store);
+
+// export const store = configureStore({
+//   reducer: {
+//     tasks: tasksReducer,
+//     filters: filtersReducer,
+//   }
+// });
