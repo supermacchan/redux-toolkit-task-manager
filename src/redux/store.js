9dbdfdfb16111f24318b4732dcce72641a8b4803
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-12 20:54:15 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,6 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "@redux-devtools/extension";
+import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./reducer";
 
-// cоздаем расширение стора чтобы добавить инструменты разработчика
-const enhancer = devToolsEnhancer();
-
-// создаем и экспортируем стор, передаем ему основной, корневой редюсер
-export const store = createStore(rootReducer, enhancer);
-// Чтобы у приложения был доступ к store, в index.js оборачиваем App в Provider и передаем ему стор в атрибуте store
+export const store = configureStore({
+  reducer: rootReducer,
+});
