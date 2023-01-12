02dc9c5ee7480d19aca65c8f09e939f5b427116f
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-12 21:45:57 +0200

    remove persist

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from "react-redux";
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from "./redux/store";
+import { store } from "./redux/store";
 import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store} >
-      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
         <App />
-      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
