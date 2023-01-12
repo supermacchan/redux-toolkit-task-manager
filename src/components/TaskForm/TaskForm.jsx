ae4a805505f15356aa70c2b1b3fa0ad6dfeda4ff
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-12 16:12:00 +0200

    Update TaskForm.jsx

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,10 +1,17 @@
+import { useDispatch } from "react-redux";
+import { addTask } from "redux/actions";
 import { Button } from "components/Button/Button";
 import css from "./TaskForm.module.css";
 
 export const TaskForm = () => {
+  const dispatch = useDispatch();
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const form = event.target;
+    // Вызываем генератор экшена и передаем текст задачи для поля payload
+    // Отправляем результат - экшен создания задачи
+    dispatch(addTask(form.elements.text.value));
     form.reset();
   };
 
