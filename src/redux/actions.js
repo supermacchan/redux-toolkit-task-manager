2a7bb67eb28f73b91ba7476bc3864c33dbd5ee10
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-12 16:00:07 +0200

    Update actions.js

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,27 @@
 // Добавить новую задачу с текстом, введенным пользователем
+export const addTask = {
+    type: "tasks/addTask",
+    payload: {
+        id: "generated id",
+        text: "entered text",
+        completed: false,
+    }
+};
+
 // Удалить задачу
+export const deleteTask = {
+    type: "tasks/deleteTask",
+    payload: "task id",
+};
+
 // Переключить статус задачи
+export const toggleCompleted = {
+    type: "tasks/toggleCompleted",
+    payload: "task id",
+};
+
 // Изменить значение фильтра статуса
+export const setStatusFilter = {
+    type: "filters/setStatusFilter",
+    payload: "filter value",
+}
