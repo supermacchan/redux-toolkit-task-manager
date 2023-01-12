b0c726686f65609bd02e53cb15d3cb1fbac1bd17
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-12 21:23:26 +0200

    Update TaskForm.jsx

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux";
-import { addTask } from "redux/actions";
+import { addTask } from "redux/tasksSlice";
 import { Button } from "components/Button/Button";
 import css from "./TaskForm.module.css";
 
