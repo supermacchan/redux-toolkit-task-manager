1ae712b3a00bf00ea25805cf772a65495eda7d8c
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-12 14:58:20 +0200

    Update TaskList.jsx

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -4,24 +4,29 @@ import { statusFilters } from "redux/constants";
 import { Task } from "components/Task/Task";
 import css from "./TaskList.module.css";
 
+// функция для фильтра задач по статусу
 const getVisibleTasks = (tasks, statusFilter) => {
   switch (statusFilter) {
     case statusFilters.active:
       return tasks.filter(task => !task.completed);
     case statusFilters.completed:
-      return tasks.filter(task => tasks.completed);
+      return tasks.filter(task => task.completed);
     default:
       return tasks;
   }
 }
 
 export const TaskList = () => {
+  // получаем все задачи
   const tasks = useSelector(selectTasks);
+  // из стейта получаем активное состояние фильтра
   const filterStatus = useSelector(selectFilter);
+  // фильтруем задачи в зависимости от выставленного фильтра
   const visibleTasks = getVisibleTasks(tasks, filterStatus);
 
   return (
     <ul className={css.list}>
+      {/* рендерим отфильтрованные задачи */}
       {visibleTasks.map(task => (
         <li className={css.listItem} key={task.id}>
           <Task task={task} />
