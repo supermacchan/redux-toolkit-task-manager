615f0733d4e7c8243bd4bcb4fa3a07143b7d3daf
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-12 21:20:45 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { tasksReducer, filtersReducer } from "./reducer";
+import { tasksReducer } from "./tasksSlice";
+import { filtersReducer } from "./filtersSlice";
 
 export const store = configureStore({
   reducer: {
